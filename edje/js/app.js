e78784d8dc14334b5694d029459e2f1622d6586f
Henner Setyono <henner.renardi@gmail.com>
2018-11-07 17:00:12 +0700

    temporary design

--- edje/js/app.js
+++ edje/js/app.js
@@ -1,63 +1,76 @@
 $( document ).ready( function() {
 
-  createSidebar();
-  parseTable();
-
-  //
-
-  // create sidebar
-  function createSidebar() {
-    var nav = {
-      'index': 'Home',
-      'grid': 'Grid & Tile',
-      'display': 'Display & Flexbox',
-      'spacing': 'Spacing',
-      'typography': 'Typography',
-      'background': 'Background',
-      'border-shadow': 'Border & Shadow',
-      'position': 'Positioning'
-    };
-
-    var navHtml = '';
-    for( var key in nav ) {
-      navHtml += `<a class="d-iblock pv-05 ph-075" href="${ key }.html">${ nav[key] }</a>`;
-    }
-
-    $( '.sidebar' ).html( '<h-grid><div class="col-12">' + navHtml + '</div></h-grid>' );
-  }
-
-  // convert JSON data to table
-  function parseTable() {
-    
-    $( 'table script' ).each( function() {
-      var content = eval( $(this).html() );
-      var $table = $(this).closest('table');
-
-      var thead = "<thead> <tr> <th>Class</th> <th>Properties</th> <th></th> </tr> </thead>";
-      
-      // create tbody
-      var tbody = "<tbody>";
-      
-      for( var i = 0, len = content.length; i < len; i++ ) {
-
-        // if empty means separator
-        if( content[i].length == 0 ) {
-          tbody += '<tr> <td class="separator" colspan="3"></td> </tr>';
-        } else {
-          // check if there's declaration (3rd param)
-          var declaration = content[i][2] ? content[i][2].join('<br>') : '';
-
-          // format class name
-          var className = content[i][0].replace( /\//g, '<br>' );
-
-          tbody += `<tr> <td>${ className }</td> <td>${ declaration }</td> <td>${ content[i][1] }</td> </tr>`;
-        }
-      }
-      tbody += "</tbody>";
-
-      $table.append( thead );
-      $table.append( tbody );
-    } );
-  }
-
+  Route.init();
+  
 });
+
+/*
+  Handle Routing
+*/
+const ROUTER = new Navigo( null, true, '#' );
+var Route = {
+  init() {
+    ROUTER.on( this.homeController.bind( this ) ).resolve();
+    ROUTER.on( '/:slug', this.pageController.bind( this ) ).resolve();
+  },
+
+  homeController() {
+    this._render( 'frontpage' );
+  },
+
+  pageController( params ) {
+    this._render( params.slug );
+  },
+
+  _render( page ) {
+    $.get( page + '.html', (data) => {
+      console.log( data );
+      $('#content').html( data );
+      parseTable();
+      parseArticle();
+    } );
+  },
+};
+
+/*
+  Convert JSON data to table
+*/
+function parseTable() {
+    
+  $( 'table script' ).each( function() {
+    var content = eval( $(this).html() );
+    var $table = $(this).closest('table');
+
+    var thead = "<thead> <tr> <th>Class</th> <th>Properties</th> <th></th> </tr> </thead>";
+    
+    // create tbody
+    var tbody = "<tbody>";
+    
+    for( var i = 0, len = content.length; i < len; i++ ) {
+
+      // if empty means separator
+      if( content[i].length == 0 ) {
+        tbody += '<tr> <td class="separator" colspan="3"></td> </tr>';
+      } else {
+        // check if there's declaration (3rd param)
+        var declaration = content[i][2] ? content[i][2].join('<br>') : '';
+
+        // format class name
+        var className = content[i][0].replace( /\//g, '<br>' );
+
+        tbody += `<tr> <td>${ className }</td> <td>${ declaration }</td> <td>${ content[i][1] }</td> </tr>`;
+      }
+    }
+    tbody += "</tbody>";
+
+    $table.append( thead );
+    $table.append( tbody );
+  } );
+}
+
+function parseArticle() {
+  $('article').each( function() {
+    var result = markdown.toHTML( $(this).html() );
+    $(this).html( result );
+  });
+}
