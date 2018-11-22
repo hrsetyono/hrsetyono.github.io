57fed511152899fc76f33f2dcd6a4fea985fa150
Henner Setyono <henner.renardi@gmail.com>
2018-11-22 15:16:12 +0700

    2.2.0

--- edje/assets/js/app.js
+++ edje/assets/js/app.js
@@ -4,11 +4,6 @@ const ROUTER = new Navigo( null, true, '#' );
 
 $( document ).ready( function() {
   Route.init();
-  
-  var host = 'hrsetyono.github.io';
-  if( (host == window.location.host) && (window.location.protocol != 'https:') ) {
-    window.location.protocol = 'https';
-  }
 });
 
 /*
@@ -45,6 +40,10 @@ var Route = {
       // scroll to top
       window.scrollTo(0, 0);
 
+      // change title tag
+      var title = data.match( /<h1>([\w\s]+)/ );
+      document.title = title[1] + ' | Edje Framework';
+
       // set active state to link
       $('.sidebar a').removeClass('active');
       $(`a[href="${ linkTarget }"]`).addClass('active');
@@ -61,7 +60,7 @@ function parseTable() {
     var content = eval( $(this).html() );
     var $table = $(this).closest('table');
 
-    var thead = "<thead> <tr> <th>Class</th> <th>Properties</th> <th></th> </tr> </thead>";
+    var thead = "<thead> <tr> <th>Name</th> <th>Properties</th> <th></th> </tr> </thead>";
     
     // create tbody
     var tbody = "<tbody>";
@@ -73,7 +72,7 @@ function parseTable() {
         tbody += '<tr> <td class="separator" colspan="3"></td> </tr>';
       } else {
         // check if there's declaration (3rd param)
-        var declaration = content[i][2] ? content[i][2].join('<br>') : '';
+        var declaration = parseDeclaration( content[i] );
 
         // format class name
         var className = content[i][0].replace( /\//g, '<br>' );
@@ -88,11 +87,26 @@ function parseTable() {
   } );
 }
 
+// Compile with markdown
 function parseArticle() {
   $('article').each( function() {
     var result = marked( $(this).html() );
     $(this).html( result );
   });
+}
+
+// Detect the content type and format it
+function parseDeclaration( raw ) {
+  // if array, join it with <br>
+  if( Array.isArray( raw[2] ) ) {
+    return raw[2].join('<br>');
+  }
+  // if ES6 string, replace newline with <br>
+  else if( raw[2] ) {
+    return raw[2].replace(/\n/g, '<br>');
+  }
+
+  return '';
 }
 
 })( jQuery );
